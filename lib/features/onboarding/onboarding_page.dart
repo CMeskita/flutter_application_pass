0bfd79fabac5b76036b5bc2944dd018424a6e38f
Msk@216582 <danielle.cmesquita@gmail.com>
2023-12-15 16:54:07 -0300

    Entrada de Dados

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_pass/commom/constants/app_color.dart';
 import 'package:flutter_application_pass/commom/constants/app_text_styles.dart';
 
-import '../../widgets/primary_button.dart';
-import '../../widgets/primary_textbutton.dart';
+import '../../commom/widgets/primary_button.dart';
+import '../../commom/widgets/primary_textbutton.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
@@ -17,50 +17,88 @@ class OnboardingPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: AppColor.colorSecundary,
       ),
-      body: Align(
-        child: Column(
-          children: [
-            const SizedBox(height: 40.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                alignment: Alignment.topRight,
-                child: Image.asset('assets/imagens/navenfeite.png'),
-              ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topRight,
+              child: Image.asset('assets/imagens/navenfeite.png'),
             ),
-            Expanded(
-              flex: 5,
-              child: Text('Acessar',
-                  style: AppTextStyler.mediumText
-                      .copyWith(color: AppColor.colorSecundary)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: PrimeryButton(
-                text: 'Enter',
-                onPressed: () {
-                  log('mensage');
-                },
-              ),
-            ),
-            PrimaryTextButton(
-              onPressed: () => log('tape'),
+          ),
+          Expanded(
+            child: Text('Acessar',
+                textAlign: TextAlign.center,
+                style: AppTextStyler.mediumText
+                    .copyWith(color: AppColor.colorSecundary)),
+          ),
+          const Form(
+            child: Column(
               children: [
-                Text(
-                  'Gurade Suas Senhas com',
-                  style: AppTextStyler.verysmallText
-                      .copyWith(color: AppColor.colorBlack),
-                ),
-                Text(
-                  ' Segurança',
-                  style: AppTextStyler.verysmallText
-                      .copyWith(color: AppColor.colorBlack),
-                ),
+                PrimaryTextFild(),
               ],
             ),
-            Image.asset('assets/imagens/footerenfeite.png'),
-            const SizedBox(height: 40.0),
-          ],
+          ),
+
+          //const TextField(),
+          PrimaryTextButton(
+            onPressed: () => log('tape'),
+            children: [
+              Text(
+                'Cadastrar',
+                textAlign: TextAlign.start,
+                style: AppTextStyler.verysmallText
+                    .copyWith(color: AppColor.colorBlack),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: PrimeryButton(
+              text: 'Enter',
+              onPressed: () {
+                log('mensage');
+              },
+            ),
+          ),
+          Image.asset('assets/imagens/footerenfeite.png'),
+          const SizedBox(height: 20.0),
+        ],
+      ),
+    );
+  }
+}
+
+class PrimaryTextFild extends StatefulWidget {
+  const PrimaryTextFild({
+    super.key,
+  });
+
+  @override
+  State<PrimaryTextFild> createState() => _PrimaryTextFildState();
+}
+
+class _PrimaryTextFildState extends State<PrimaryTextFild> {
+  final defaultBorder = const OutlineInputBorder();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Email",
+        focusedBorder: defaultBorder.copyWith(
+          borderSide: const BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        errorBorder: defaultBorder,
+        focusedErrorBorder: defaultBorder,
+        enabledBorder: defaultBorder,
+        disabledBorder: defaultBorder.copyWith(
+          borderSide: const BorderSide(
+            color: Colors.yellow,
+          ),
         ),
       ),
     );
