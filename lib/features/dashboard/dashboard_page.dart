b90ce336ea0407c9dc24008d19f6291194cdffec
Msk@216582 <danielle.cmesquita@gmail.com>
2023-12-20 16:34:33 -0300

    dash continuidade

--- lib/features/dashboard/dashboard_page.dart
+++ lib/features/dashboard/dashboard_page.dart
@@ -1,47 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_pass/commom/constants/app_text_styles.dart';
-
-import '../../commom/constants/app_color.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.colorWhite,
-      appBar: AppBar(
-        backgroundColor: AppColor.colorSecundary,
-      ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 20.0),
-          const NewWidget(),
-          Text(
-            textAlign: TextAlign.center,
-            'DashBoard',
-            style:
-                AppTextStyler.mediumText.copyWith(color: AppColor.colorBlack),
-          )
-        ],
-      ),
+    return const Scaffold(
+      body: Dashboard(),
     );
   }
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        alignment: Alignment.topRight,
-        child: Image.asset('assets/imagens/navenfeite.png'),
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // Defina o número de colunas aqui
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
       ),
+      itemCount:
+          6, // Defina o número total de células na grade (linhas x colunas)
+      itemBuilder: (BuildContext context, int index) {
+        // Aqui você pode criar os widgets individuais para cada célula
+        return Card(
+          child: Center(
+            child: Text('Item $index'),
+          ),
+        );
+      },
     );
   }
 }
